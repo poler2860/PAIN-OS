d83de706cdb15ac0b4f4eafe5c42dc42112d0e0b
Polychronis <poler2860@gmail.com>
2024-08-14 00:21:30 +0300

    A bit of clarity for thy sanity

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -13,8 +13,7 @@ mov     sp, bp
 
 jmp boot
 
-; ------ GDT ------
-
+%import
 
 boot:
     cli         ; Disable all interrupts
@@ -32,44 +31,6 @@ boot:
     mov     cs, ax
 
     hlt
-
-load_kernel:
-; TODO: kernel load
-; What did you expect
-
-
-; DL is inserted as a parameter
-disk_read:
-    pusha
-    push    dx                      ; Save number of sectors to read
-
-    ; Read sector into memory
-    mov     ah, 0x2                 ; Disk read function code
-    mov     al, 0x2                 ; Sectors to read
-
-    mov     ch, 0x0                 ; (Lower 8 bits) cylinder number
-    mov     cl, 0x2                 ; Sector number
-    mov     dh, 0x0                 ; Head number
-    int     13h
-    jc      disk_error              ; Check carry bit for disk read error
-
-    pop     dx                      ; Get the origial number of sector to read
-    cmp     al, dh                  ; Compare the sectors actuall read
-                                    ; to the sectors wanted to read
-    jne     sectors_error           ; Re-read the sectors if we could not read all the sectors
-
-    popa
-    ret
-
-disk_error:
-    jmp     disk_loop
-
-sectors_error:
-    jmp     disk_loop
-
-disk_loop:
-    jmp     $
-
 
 BOOT_DRIVE  db  0                   ; Set boot drive number
 
