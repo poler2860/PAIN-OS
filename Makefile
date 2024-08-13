442e91227ebf37c92b470b88054ee230a170a5c8
Polychronis <poler2860@gmail.com>
2024-08-14 01:26:43 +0300

    Some more boot shitshow code

--- Makefile
+++ Makefile
@@ -1,15 +1,15 @@
 CC = i686-elf-gcc
 
-all: bootloader run
+all: kernel bootloader run
 
 kernel: src/kernel/kernel.c
 	$(CC) -c $^ -o bin/kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 
 bootloader: src/bootloader/boot.asm
-	nasm -f bin $^ -o bin/boot.bin
+	nasm -f bin $^ -o bin/boot.bin -I src/bootloader/include
 
 run: bin/boot.bin
-	qemu-system-i386 $^ -net none
+	qemu-system-i386 $< -net none
 
 clean:
 	rm -f bin/boot.bin bin/kernel.o
--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -1,40 +1,69 @@
-; Set origin after the the BIOS table
 [bits 16]
 [org 7c00h]
 
 ; Set the location to load the kernel into the memory
 KERNEL_OFFSET   equ   0x1000
 
+; Save the boot drive number for later
 mov     [BOOT_DRIVE], dl
 
 ; Setup the stack
 mov     bp, 0x9000
 mov     sp, bp
 
-jmp boot
+jmp     boot
 
-%import
+%include "disk.asm"
+%include "gdt.asm"
 
 boot:
-    cli         ; Disable all interrupts
 
-    ; Setup segment registers
-    ; Using the flat protected model
-    xor     ax, ax
-    mov     ss, ax
+    call    load_kernel
+    call    switch_to_32bit
+
+    call BEGIN_32bit
+
+[bits 32]
+BEGIN_32BIT:
+    call KERNEL_OFFSET      ; give control to the kernel
+    hlt
+
+[bits 16]
+load_kernel:
+    mov bx, KERNEL_OFFSET   ; bx -> destination
+    mov dh, 2               ; dh -> num sectors
+    mov dl, [BOOT_DRIVE]    ; dl -> disk
+    call disk_read
+    ret
+
+switch_to_32bit:
+    cli                             ; Disable interrupts
+    lgdt    [gdt_descriptor]        ; Load descriptor table
+
+    ; Enable protected mode
+    mov     eax, cr0
+    or      cr0, 0x1
+    mov     cr0, eax
+
+    jmp     CODE_SEG:init_prot
+    ret
+
+init_prot:
+
+    ; Setup the segment registers
+    mov     ax, DATA_SEG
     mov     ds, ax
+    mov     ss, ax
     mov     es, ax
     mov     fs, ax
     mov     gs, ax
 
-    mov     ax, 0xFFFFFFFF
-    mov     cs, ax
+    mov     ebp, 0x90000        ; Setup stack
+    mov     esp, ebp
 
-    hlt
+    ret
 
 BOOT_DRIVE  db  0                   ; Set boot drive number
 
-; Fill the rest of the 512 bytes but the last 4 bytes with zeros and make the sector bootable
-fill:
-    times 510-($-$$) db 0
-    db 0x55, 0xAA
+times 510-($-$$) db 0               ; Add padding to the rest of the sector
+db 0x55, 0xAA                       ; Make it bootable
